3a64dfe59d2ab06db1fb8af1d8e3b7dba67b87e3
MohamedAmineM <96208160+MohamedAmineM@users.noreply.github.com>
2025-10-20 20:01:32 +0200

    Modify environment.prod.ts API URL configuration
    Updated API URL to use relative path for nginx redirection.

--- madrasatiFront/src/environments/environment.prod.ts
+++ madrasatiFront/src/environments/environment.prod.ts
@@ -1,4 +1,9 @@
-export const environment = {
+/*export const environment = {
     production: true,
     apiurl: 'http://madrasati-backend:8080/api'
-}
+}*/
+
+export const environment = {
+    production: true,
+    apiurl: '/api'   // relative path redirection with nginx
+};
