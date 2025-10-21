b13c437fb5ee1e62bb5186c56aad5008d0b06c7a
MohamedAmineM <96208160+MohamedAmineM@users.noreply.github.com>
2025-10-21 17:46:29 +0200

    Update environment.ts

--- madrasatiFront/src/environments/environment.ts
+++ madrasatiFront/src/environments/environment.ts
@@ -3,7 +3,12 @@
     apiurl: 'http://localhost:8080/api'
 }*/
 
-export const environment = {
+/*export const environment = {
     production: false,
     apiurl: 'api/'   // relative path redirection with nginx
-};
+};*/
+
+export const environment = {
+    production: false,
+    apiurl: 'http://backend:8082/api'
+}
